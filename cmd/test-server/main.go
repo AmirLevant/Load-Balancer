e71aabcb962b9d4c669cde49c9d30933f441d010
AmirLevant <amircan206@gmail.com>
2026-03-02 22:41:23 +0200

    Fix: fixed test server issues

--- cmd/test-server/main.go
+++ cmd/test-server/main.go
@@ -6,84 +6,88 @@ import (
 	"io"
 	"log/slog"
 	"net"
+	"os"
 
 	"github.com/BurntSushi/toml"
 )
 
 type serverConfig struct {
-	Server_port string `toml:"server_port"`
+	ServerPort string `toml:"server_port"`
 }
 
 func main() {
-
 	var cfg serverConfig
-
 	if _, err := toml.DecodeFile("server.toml", &cfg); err != nil {
-		slog.Error("failed to decode server toml", slog.Any("error", err))
+		slog.Error("Failed to decode server toml", slog.Any("error", err))
 		return
 	}
 
-	StartServer(cfg.Server_port)
+	err := StartServer(cfg.ServerPort)
+	if err != nil {
+		slog.Error("Failed starting client", slog.Any("error", err))
+		os.Exit(1)
+	}
 }
 
-func StartServer(port string) {
+func StartServer(port string) error {
 
-	// we listen on server port
+	// Listen on server port
 	listener, err := net.Listen("tcp", ":"+port)
 
 	if err != nil {
-		fmt.Println("Error listening:", err)
-		return
+		return fmt.Errorf("Failed listening: %w", err)
 	}
 
 	defer listener.Close()
 
-	fmt.Println("Server running on port :" + port)
+	slog.Info("Server running on port :" + port)
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Error Accepting:", err)
+			slog.Error("Error Accepting:", slog.Any("error", err))
 			continue
 		}
-		fmt.Println("server " + port + " has recieved a connection")
-		go handleConnection(conn)
+		slog.Info("server " + port + " has recieved a connection")
+		go func() {
+			if err := handleConnection(conn); err != nil {
+				slog.Error("Failed handling connection", slog.Any("error", err))
+			}
+		}()
 	}
 }
 
-func handleConnection(conn net.Conn) {
-	// always close the connection at the end
+func handleConnection(conn net.Conn) error {
+	// Always close the connection at the end
 	defer func() {
 		fmt.Println("Closing connection")
 		err := conn.Close()
 		if err != nil {
-			fmt.Println("Failed closing connection:", err)
+			slog.Error("Failed closing connection:", slog.Any("error", err))
 		} else {
-			fmt.Println("Connection closed")
+			slog.Info("Connection closed")
 		}
 	}()
 
-	// len of data at the start of the buffer, 4 bytes
-	Rxbuffer := make([]byte, 4)
-	Txbuffer := make([]byte, 4)
+	// Len of data at the start of the buffer, 4 bytes
+	rxBuffer := make([]byte, 4)
+	txBuffer := make([]byte, 4)
 
 	for i := 0; i < 10; i++ {
-
-		// read the message
-		_, err := conn.Read(Rxbuffer)
+		// Read the message
+		_, err := conn.Read(rxBuffer)
 		if err != nil && err != io.EOF {
-			fmt.Println("Error reading:", err)
-			return
+			return fmt.Errorf("Error reading: %w", err)
 		}
-		msg := binary.LittleEndian.Uint32(Rxbuffer)
-		fmt.Printf("Message Content is: %d \n", msg)
+		msg := binary.LittleEndian.Uint32(rxBuffer)
+		slog.Info("Message Content is: %d \n", msg)
 
-		// write back
+		// Write back
 		msg = msg + 3
-		binary.LittleEndian.PutUint32(Txbuffer, msg)
-		_, err = conn.Write(Txbuffer)
+		binary.LittleEndian.PutUint32(txBuffer, msg)
+		_, err = conn.Write(txBuffer)
 		if err != nil {
-			fmt.Println("Error writing:", err)
+			slog.Info("Error writing:", err)
 		}
 
 	}
